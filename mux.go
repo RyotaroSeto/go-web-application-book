456ef0baa41f1c13760e008e29b11c0aec8821c6
RyotaroSeto <hahahahawaiwai321@gmail.com>
2023-09-28 01:37:03 +0900

    complete section075

--- mux.go
+++ mux.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"context"
 	"net/http"
 
+	"go_todo_app/clock"
+	"go_todo_app/config"
 	"go_todo_app/handler"
 	"go_todo_app/store"
 
@@ -10,7 +13,7 @@ import (
 	"github.com/go-playground/validator/v10"
 )
 
-func NewMux() http.Handler {
+func NewMux(ctx context.Context, cfg *config.Config) (http.Handler, func(), error) {
 	mux := chi.NewRouter()
 	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json; charset=utf-8")
@@ -19,10 +22,17 @@ func NewMux() http.Handler {
 	})
 
 	v := validator.New()
-	at := &handler.AddTask{Store: store.Tasks, Validator: v}
+	db, cleanup, err := store.New(ctx, cfg)
+	if err != nil {
+		return nil, cleanup, err
+	}
+	r := store.Repository{Clocker: clock.RealClocker{}}
+
+	at := &handler.AddTask{DB: db, Repo: &r, Validator: v}
 	mux.Post("/tasks", at.ServeHTTP)
 
-	lt := &handler.ListTask{Store: store.Tasks}
+	lt := &handler.ListTask{DB: db, Repo: &r}
 	mux.Get("/tasks", lt.ServeHTTP)
-	return mux
+
+	return mux, cleanup, nil
 }
