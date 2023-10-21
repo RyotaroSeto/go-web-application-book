0832182f19b9d58ba0b5d4ba0f766af554a4f47e
Ryotaro <70475997+RyotaroSeto@users.noreply.github.com>
2023-10-21 16:03:49 +0900

    complete section85 (#15)
    * complete section85
    
    * comment out jwt test reason pem file

--- mux.go
+++ mux.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"net/http"
 
+	"go_todo_app/auth"
 	"go_todo_app/clock"
 	"go_todo_app/config"
 	"go_todo_app/handler"
@@ -21,28 +22,35 @@ func NewMux(ctx context.Context, cfg *config.Config) (http.Handler, func(), erro
 		// 静的解析のエラーを回避するため明示的に戻り値を捨てている
 		_, _ = w.Write([]byte(`{"status": "ok"}`))
 	})
-
 	v := validator.New()
 	db, cleanup, err := store.New(ctx, cfg)
 	if err != nil {
 		return nil, cleanup, err
 	}
-	r := store.Repository{Clocker: clock.RealClocker{}}
-	at := &handler.AddTask{
-		Service:   &service.AddTask{DB: db, Repo: &r},
-		Validator: v,
+	clocker := clock.RealClocker{}
+	r := store.Repository{Clocker: clocker}
+	rcli, err := store.NewKVS(ctx, cfg)
+	if err != nil {
+		return nil, cleanup, err
 	}
-	mux.Post("/tasks", at.ServeHTTP)
-
-	lt := &handler.ListTask{
-		Service: &service.ListTask{DB: db, Repo: &r},
+	jwter, err := auth.NewJWTer(rcli, clocker)
+	if err != nil {
+		return nil, cleanup, err
 	}
-	mux.Get("/tasks", lt.ServeHTTP)
-
 	ru := &handler.RegisterUser{
 		Service:   &service.RegisterUser{DB: db, Repo: &r},
 		Validator: v,
 	}
 	mux.Post("/register", ru.ServeHTTP)
+	l := &handler.Login{
+		Service: &service.Login{
+			DB:             db,
+			Repo:           &r,
+			TokenGenerator: jwter,
+		},
+		Validator: v,
+	}
+	mux.Post("/login", l.ServeHTTP)
+
 	return mux, cleanup, nil
 }
